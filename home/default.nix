f4adc798ed510f56ee034e18079b330fe31f5450
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-12-20 20:23:29 +0900

    fix: enable XDG for proper lazygit config location

--- home/default.nix
+++ home/default.nix
@@ -13,6 +13,9 @@
   home.username = "kokiyamaguchi";
   home.homeDirectory = "/Users/kokiyamaguchi";
 
+  # Enable XDG directories
+  xdg.enable = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
