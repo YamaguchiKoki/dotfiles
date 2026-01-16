beda4b2b6820acd02b33e9ba8d28b2189397d1a3
YamaguchiKoki <shankouhanghui15@gmail.com>
2026-01-16 12:33:48 +0900

    fix: a

--- home/default.nix
+++ home/default.nix
@@ -101,6 +101,7 @@
     # yazi / helix / lazygit 連携設定
     ".config/helix/config.toml".source = ./dotfiles/helix/config.toml;
     ".config/helix/languages.toml".source = ./dotfiles/helix/languages.toml;
+    ".config/helix/runtime".source = ./dotfiles/helix/runtime;
     ".config/yazi/keymap.toml".source = ./dotfiles/yazi/keymap.toml;
     ".config/yazi/yazi.toml".source = ./dotfiles/yazi/yazi.toml;
   };
